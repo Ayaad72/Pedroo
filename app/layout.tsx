accb2d1128df05fdff7dabdb52f563f8f6f1920c
Ayaad72 <ayaadalavi@gmail.com>
2025-01-22 22:22:53 +0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { ReactLenis } from "lenis/react";
+
+import { Manrope } from "next/font/google";
+import "./globals.css";
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+export const metadata: Metadata = {
+  title: "PedroCoin - Building the Future of Memes & Tokens",
+  description: "Where humor and innovation unite on the Solana blockchain",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={manrope.className}>
+        <ReactLenis root>{children}</ReactLenis>
+      </body>
+    </html>
+  );
+}
