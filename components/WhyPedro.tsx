0f124be3924103b6d563aa54f16a7b446f90737c
Ayaad72 <ayaadalavi@gmail.com>
2025-01-24 17:28:50 +0500

    added some animations

--- components/WhyPedro.tsx
+++ components/WhyPedro.tsx
@@ -66,8 +66,8 @@ export function WhyPedro() {
           }
         >
           pedoro is not just dfsdfsf a asdasdass isn't just another pump.shit
-          coin. It's the quirky, dasdas-powered token built on Solana to bring
-          memes and utility together for the long haul. 🚀🐒
+          coin. It'dasdasd the quirky, dasdas-powered token built on Solana to
+          bring memes and utility together for the long haul. 🚀🐒
         </p>
       </div>
 
